b85aca359bca61e0230d414fac1ce05c1a81b594
Daniel Choi <dan@choibles.com>
2015-07-14 08:34:04 -0700

    upated summarizer

--- summarizer.php
+++ summarizer.php
@@ -1,53 +1,14 @@
 <?php
 
-	/*
-	Infobox - content
-	Overview
-
-	// duke energe
-	Type
-	Traded as
-	Industry
-	Founded
-	Headquarters
-	Area served
-	Key people
-	Products
-	Revenue
-	Operating Income
-	Net Income
-	Total assets
-	Total equity
-	Number of employees
-	Website
-
-	// college
-	Motto
-	Motto in English
-	Established
-	Type
-	Endowment
-	President
-	Provost
-	Academic staff
-	Undergraduates
-	Postgraduates
-	Location
-	Campus
-	Colors
-	Athletics
-	Sports
-	Nickname
-	Mascot
-	Affiliations
-	Website
-	*/
+	// https://en.wikipedia.org/w/api.php?action=query&titles=Duke%20Energy&prop=info|revisions&rvprop=content&format=json
 
 	$industries = array(
-		'universities' => 
-		'private schools' =>
-		'utilities' =>
-		
+		'company',
+		'law firm',
+		'organization',
+		'school',
+		'university',
+		'venue'		
 	)
 	[Name] is an [industry] company headquartered in [headquarters].
 
