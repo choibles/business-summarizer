ee6e04b5d171d53bb28564f7e0754dda3fe94331
Daniel Choi <dan@choibles.com>
2015-07-15 23:07:03 -0700

    Fixed summarizer for deep nesting

--- summarizer.php
+++ summarizer.php
@@ -1,7 +1,6 @@
 <?php
 
 	// https://en.wikipedia.org/w/api.php?action=query&titles=Duke%20Energy&prop=info|revisions&rvprop=content&format=json
-	
 	/*
 	$url = 'https://en.wikipedia.org/w/api.php?action=query&titles=Duke%20Energy&prop=info|revisions&rvprop=content&format=json';
 	
@@ -38,12 +37,14 @@
 		preg_match_all('/\|\s*([\w_]*)\s*=\s*(.*?)\n/', $infobox, $infobox_data_matches, PREG_SET_ORDER);
 		foreach ($infobox_data_matches as $infobox_field)
 		{
-			$wikipage['properties'][$infobox_field[1]] = $infobox_field[2];
+			$altered_value = str_replace(array('</ref>'),array(''),preg_replace('/<ref.*?>/','',$infobox_field[2]));
+			$wikipage['properties'][$infobox_field[1]] = $altered_value;
 		}
 	}
+	//print_r($page);
+	//echo "\n\n------\n\n";
+	print_r($wikipage);*/
 
-	print_r($wikipage);
-*/
 	$company = array(
 		'name' => 'Dan\'s Microwaves',
 		'former_name' => 'Mitch Mics',
@@ -57,55 +58,31 @@
 		// exit and error out
 		exit;
 	}
-
 	$template = file_get_contents($template_filepath);
 
-	echo evaluate($template, $company);
+	echo evaluate_template($template, $company);
 
-
-	/*
-
-	$business_types = array(
-		'company',
-		'law firm',
-		'organization',
-		'school',
-		'university',
-		'venue'		
-	);
-
-	$business_name = array();
-
-	$business_name['subject'] = array(
-		$business['name'],
-		'the '.$business['business_type'],
-		'it'
-	);
-
-	$business_name['object'] = array(
-		$business['name'],
-		'the '.$business['business_type']
-	);*/
-
-	function evaluate($string, $parameters)
+	function evaluate_template($template, $parameters)
 	{
-		// find all \token{string} matches
-		preg_match_all('/(?<token>
-        \{[^{}]*((?P>token)[^{}]*)?\} # {something}
-)/x', $string, $matches, PREG_OFFSET_CAPTURE);
-
+		// find all {string} matches
+		preg_match_all('/\{((([^\{\}]+)|(?R))*)\}/', $template, $matches, PREG_OFFSET_CAPTURE);
 		$last_offset = 0;
 		$new_string = '';
-		// for each \token{string} match, evaluate and concatenate all text before the match and also the match itself
-		foreach ($matches[0] as $index=>$match)
+		// for each {string} match, evaluate and concatenate all text before the match and also the match itself
+		foreach ($matches[0] as $match)
 		{
 			// grab a couple initial state variables
 			$substring = $match[0];
+			$content = substr($substring, 1, strlen($substring)-2); // substring without enclosing braces
 			$offset = $match[1];
 			$length = strlen($substring);
 
+			// remove sub-matches so we only check this level for requirements
+			preg_match_all('/\{((([^\{\}]+)|(?R))*)\}/', $content, $obscure_matches);
+			$test_substring = str_replace($obscure_matches[0], array(), $substring);
+			
 			// look for all placeholders, set as requirements
-			preg_match_all('/\*(.*?)\*/', $substring, $requirements_matches);
+			preg_match_all('/\*(.*?)\*/', $test_substring, $requirements_matches);
 			$has_requirements = true;
 			foreach ($requirements_matches[1] as $requirement)
 			{
@@ -113,26 +90,56 @@
 			}
 
 			// process the actual match
-			$first_bracket_offset = strpos($substring, "{");  // start of string in \token{string}
-			$last_bracket_offset = strrpos($substring, "}");  // end of string in \token{string}
-			if ($has_requirements)
-			{
-				$substring = evaluate(substr($substring,$first_bracket_offset+1,$last_bracket_offset-$first_bracket_offset-1), $parameters);
-			}
+			if ($has_requirements) $substring = evaluate_template($content, $parameters);
 			else $substring = '';
 
-			// all text before the \token{string} match
-			$new_string .= substr($string, $last_offset, $offset-$last_offset);
+			// all text before the {string} match
+			$new_string .= substr($template, $last_offset, $offset-$last_offset);
 
-			// the evaluated \token{string} match
+			// the evaluated {string} match
 			$new_string .= $substring;
 
-			// set the last offset – where we should pick up after the last \token{string} match
+			// set the last offset – where we should pick up after the last {string} match
 			$last_offset = $offset+$length;
 		}
 
-		// add everything in the $string after the last match
-		$new_string .= substr($string, $last_offset, strlen($string)-$last_offset);
+		// add everything in the $template after the last match
+		$new_string .= substr($template, $last_offset, strlen($template)-$last_offset);
 		return str_replace(array_map(function($x) { return '*'.$x.'*'; }, array_keys($parameters)), array_values($parameters), $new_string);
 	}
+
+/*
+
+	
+
+	print_r(template_braces($template));
+
+	function template_braces($string, $recursion = 0) {
+		$results = array();
+	    if (preg_match_all("/\{((([^\{\}]+)|(?R))*)\}/", $string, $matches, PREG_SET_ORDER|PREG_OFFSET_CAPTURE)) {
+	        foreach ($matches as $match) {
+	            $results[] = array('match' => $match[0][0], 'offset' => $match[0][1], 'deepness' => $recursion);
+	            if ($InnerResults = template_braces($match[1][0], $recursion+1)) {
+	                $results = array_merge($results, $InnerResults);
+	            }
+	        }
+	        return $results;
+	    }
+	    return false;
+	}
+
+
+	function wikipedia_braces($string, $recursion = 0) {
+	    $results = array();
+	    if (preg_match_all("/\{\{(?<tagname>[\w\s]+)(?<content>(([^\{\}]+)|(?R))*)\}\}/", $string, $matches, PREG_SET_ORDER|PREG_OFFSET_CAPTURE)) {
+	        foreach ($matches as $match) {
+	            $results[] = array('match' => $match[0], 'tagname' => $match['tagname'], 'content' => $match['content'], 'deepness' => $recursion);
+	            if ($InnerResults = wikipedia_braces($match['content'], $recursion+1)) {
+	                $results = array_merge($results, $InnerResults);
+	            }
+	        }
+	        return $results;
+	    }
+	    return false;
+	}*/
 ?>
